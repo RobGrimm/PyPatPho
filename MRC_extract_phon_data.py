81fb342543b0f00fa41a2e1cecd1a271a9325c11
stephantul <stephantulkens@stephans-MacBook-Pro.local>
2015-03-23 22:13:53 +0100

    added MRCRepo class
    Added MRCRepo class, the pickle is now an instance of MRCRepo, which is
    just a wrapper around a dict with some extra methods

--- MRC_extract_phon_data.py
+++ MRC_extract_phon_data.py
@@ -1,11 +1,44 @@
 import os
 import cPickle
 import time
+from collections import defaultdict
 from xlrd import open_workbook
+
+
+class MRCRepo(object):
+    """class that represents the MRC database, organised by POS"""
+
+    def __init__(self, lexemes):
+
+        self.lexemes = lexemes
+
+    def phonologicalform_word(self, word):
+
+        for pos in self.lexemes.keys():
+
+            try:
+                return pos[word]
+            except KeyError:
+                continue
+        return None
+
+    def phonologicalform_pos(self, pos, word):
+
+        try:
+            return self.lexemes[pos][word]
+        except KeyError:
+            return None
+
+    def allwordsbypos(self, pos):
+
+        try:
+            return self.lexemes[pos]
+        except KeyError:
+            return {}
+
 
 """ Hacked-together script that goes through the MRC XLS file and extracts phonological represenations of nouns, verbs, adjectives.
 Stores the resuls in a dict and pickles it to file. Requires MRC database: http://www.psych.rl.ac.uk/Word_data.zip """
-
 def get_words_from_mrc(pathtodir, pos={'N': 'n', 'J': 'adj', 'V': 'v'}):
 
     start_time = time.time()
@@ -16,7 +49,7 @@ def get_words_from_mrc(pathtodir, pos={'N': 'n', 'J': 'adj', 'V': 'v'}):
     print "Starting"
     # a lexeme is a word's orthographic representation (I think this is the lemma, the MRC documntation is not clear on this)
     # together with its POS tag, e.g. 'car-n' for the noun 'car'
-    lexeme_dict = dict()
+    lexeme_dict = {}
 
     # get this from: http://www.psych.rl.ac.uk/Word_data.zip
 
@@ -49,15 +82,18 @@ def get_words_from_mrc(pathtodir, pos={'N': 'n', 'J': 'adj', 'V': 'v'}):
                 continue
 
             # add phonological representation to lexeme dict
-            lexeme = word + '-' + pos_dict[pdwtype]
-            lexeme_dict[lexeme] = phonemes
 
-    cPickle.dump(lexeme_dict, open(pathtodir + '/mrc_dict.p', "wb"))
+            lexeme_dict.setdefault(pos_dict[pdwtype], dict())[word] = phonemes
+            '''lexeme_dict.setdefault(pdwtype, dict())[word] = phonemes'''
 
     print "--- Done. This took %s minutes ---" % ((time.time() - start_time) / float(60))
 
     print 'Extracted %s words.' % len(lexeme_dict) # 33.938 nouns, verbs and adjectives
 
+    return MRCRepo(lexeme_dict)
+
+
 if __name__ == "__main__":
 
-    get_words_from_mrc(os.getcwd())
+    mrc = get_words_from_mrc(os.getcwd())
+    cPickle.dump(mrc, open(os.getcwd() + '/mrc.p', "wb"))

7583930d47e809b93a83bc219511e50ee3fc60dc
RobGrimm <robert.grimm@uantwerpen.be>
2018-07-25 10:51:06 +0200

    shorter lines

--- MRC_extract_phon_data.py
+++ MRC_extract_phon_data.py
@@ -15,7 +15,8 @@ the MRC phonemes that need to be converted are:
 'o'  (p_o_t) -- only uses'''
 CONVERT = {'e': 'E', '0': 'Q', '9': 'N', 'tS': 'C', 'dZ': 'J', 'o': 'Q'}
 
-# additional chars in the DPHON field in the MRC database which need to be stripped from the phonological form
+# additional chars in the DPHON field in the MRC database
+# which need to be stripped from the phonological form
 REMOVE = {'2', 'Q', '-', '+', 'R', "'"}
 
 
@@ -26,19 +27,25 @@ def replace_all(text, dic):
 
 
 def get_words_from_mrc(pathtodir):
-    """ Function that goes through the MRC XLS file and extracts phonological representations """
+    """ Function that goes through the MRC XLS file and extracts
+    phonological representations """
 
     start_time = time.time()
 
     # map the MRC POS tags to more easily readable tags
-    pos_conversion_dict = {'N': 'n', 'J': 'adj', 'V': 'v', 'A': 'adv', 'R': 'prep', 'C': 'conj', 'U': 'pron',
-                           'I': 'interj', 'P': 'past_part', 'O': 'other'}
+    pos_dict = {'N': 'n', 'J': 'adj', 'V': 'v', 'A': 'adv',
+                'R': 'prep', 'C': 'conj', 'U': 'pron',
+                'I': 'interj', 'P': 'past_part', 'O': 'other'}
 
-    #dictionary for mapping orthographic to phonological representations, organized by WTYPE vs. PDWTYOE and POS tag
-    #WTYPE is a more fine-grained POS tag scheme; a word's PDWTYPE is its most common POS tag
-    #if you want the phonology for a word's WTYPE but can't find it, you may want to go with its PDWTYPE instead
+    # dictionary for mapping orthographic to phonological representations,
+    # organized by WTYPE vs. PDWTYPE and POS tag.
+    # WTYPE is a more  fine-grained POS tag scheme:
+    # a word's PDWTYPE is its most common POS tag.
+    # if you want the phonology for a word's WTYPE but can't find it,
+    # you may want to go with its PDWTYPE instead
     mrc_dict = {'WTYPE':
-                    {'n': {},  'adj': {},  'v': {},  'adv': {},  'prep': {},  'conj': {}, 'pron': {}, 'interj': {},
+                    {'n': {},  'adj': {},  'v': {},  'adv': {},  'prep': {},
+                     'conj': {}, 'pron': {}, 'interj': {},
                      'past_part': {}, 'other': {}},
 
                 'PDWTYPE':
@@ -53,10 +60,10 @@ def get_words_from_mrc(pathtodir):
     for row in range(s.nrows):
 
         # take the orthographic form from the 'WORD' field
-        orthographic_form = str(s.cell(row, s.ncols - 4).value).lower()
-        orthographic_form = orthographic_form.strip() # strip whitespace
+        orthographic = str(s.cell(row, s.ncols - 4).value).lower()
+        orthographic = orthographic.strip() # strip whitespace
 
-        if not orthographic_form:
+        if not orthographic:
             continue
 
         # take the POS tag from the 'WTYPE' field
@@ -65,7 +72,8 @@ def get_words_from_mrc(pathtodir):
         if not wtype:
             continue
 
-        # take the phonological form from the DPHON field (not every word has an entry for the PHON field)
+        # take the phonological form from the DPHON field
+        # (not every word has an entry for the PHON field)
         phonemes = str(s.cell(row, s.ncols - 2).value)
         phonemes = ''.join([a for a in phonemes if a not in REMOVE])
         phonemes = replace_all(phonemes, CONVERT).strip()
@@ -73,21 +81,24 @@ def get_words_from_mrc(pathtodir):
         if not phonemes:
             continue
 
-        # sometimess the WTYPE of a word is erroneous, so check first before adding to 'mrc_dict'
+        # sometimess the WTYPE of a word is erroneous,
+        # so check first before adding to 'mrc_dict'
         try:
-            mrc_dict['WTYPE'][pos_conversion_dict[wtype]][orthographic_form] = phonemes
+            mrc_dict['WTYPE'][pos_dict[wtype]][orthographic] = phonemes
         except KeyError:
             pass
 
-        # if the word has an entry for 'PDWTYPE' (the most common POS tag for that word), get it
+        # if the word has an entry for 'PDWTYPE'
+        # (the most common POS tag for that word), get it
         pdwtype = str(s.cell(row, s.ncols - 10).value)
 
         try:
-            mrc_dict['PDWTYPE'][orthographic_form] = (phonemes, pos_conversion_dict[pdwtype])
+            mrc_dict['PDWTYPE'][orthographic] = (phonemes, pos_dict[pdwtype])
         except KeyError:
             continue
 
-    print("--- Done. This took %s minutes ---" % ((time.time() - start_time) / float(60)))
+    print("--- Done. This took %s minutes ---" %
+          ((time.time() - start_time) / float(60)))
 
     return mrc_dict
 
